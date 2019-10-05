cb7c7545e8089dc4f6b277e1441d6e108645c5ae
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-10-05 14:31:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,7 @@ class TicTacToe
     user_input.to_i - 1
   end
   
-  def move(board, index, current_player = "X")
-  @board[index] = current_player
+  def move(position, char)
+    @board[position] = char
   end
 end
